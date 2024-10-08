dd500af48e40da9aaf1fdcc10bc60e62c32a42c1
raptor0999 <raptordev@proton.me>
2024-10-08 11:18:20 -0400

    Initial commit

---
+++ main.gd
@@ -0,0 +1,50 @@
+extends Node3D
+
+@onready var level_node:Node3D = $Level
+@onready var fade_rect:ColorRect = $CanvasLayer/ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Globals.connect("load_level", load_level)
+	load_level("level1", 0.0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func load_level(level_name: String, player_x: float):
+	clear_current_level()
+	print("Cleared current level")
+	
+	do_level_transition()
+	
+	var level_scene = load("res://levels/" + level_name + ".tscn")
+	var level_instance = level_scene.instantiate()
+	
+	level_node.add_child(level_instance)
+	print("Loaded level named: " + level_name)
+	
+	spawn_player(player_x)
+	print("Spawned player")
+	
+func clear_current_level():
+	for l in level_node.get_children():
+		l.queue_free()
+		
+func do_level_transition():
+	print("Doing level transition")
+	#var screen_size = get_viewport().get_visible_rect().size
+	#fade_rect.size = screen_size
+	var tween:Tween = create_tween()
+	tween.tween_property(fade_rect, "modulate:a", 0.0, 5.0).from(1.0)
+	tween.play
+	await tween.finished
+	print("Transition finished")
+	
+func spawn_player(player_x: float):
+	var player_scene = load("res://scenes/characters/player/player.tscn")
+	var player_instance = player_scene.instantiate()
+	player_instance.position.x = player_x
+	print("Player X: " + str(player_x))
+	
+	level_node.add_child(player_instance)
---
+++ scenes/characters/player/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+@onready var area:Area3D = $Area3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	# Handle quit
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit(0)
+		
+	# Handle doors.
+	if Input.is_action_just_pressed("door") and is_on_floor():
+		testForDoor()
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_axis("move_left", "move_right")
+	var direction := (transform.basis * Vector3(input_dir, 0, 0)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+func testForDoor():
+	for a in area.get_overlapping_areas():
+		Globals.load_level.emit(a.get_parent().to_level, a.global_position.x)
